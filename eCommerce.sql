1b13e47bbfa109a69c8dfabe71b56d037b9d7eb3
Karthi1945 <167897358+Karthi1945@users.noreply.github.com>
2025-04-02 19:50:21 +0530

    created the table need to update the query

---
+++ eCommerce.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: sql12.freesqldatabase.com
+-- Generation Time: Apr 02, 2025 at 02:17 PM
+-- Server version: 5.5.62-0ubuntu0.14.04.1
+-- PHP Version: 7.0.33-0ubuntu0.16.04.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sql12770881`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eCommerce`
+--
+-- Error reading structure for table sql12770881.eCommerce: #1146 - Table 'sql12770881.eCommerce' doesn't exist
+-- Error reading data for table sql12770881.eCommerce: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'FROM `sql12770881`.`eCommerce`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
